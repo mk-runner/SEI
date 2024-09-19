943582e4122c7ef9c196d8b4b59ed424b44cc74d
Kang Liu <719176062@qq.com>
2024-09-19 17:29:32 +0800

    Update main_v0201.py

--- main_v0201.py
+++ main_v0201.py
@@ -12,11 +12,11 @@ from modules.dataloaders import PretrainLoader, FinetuneLoaderHaveIndication, Fi
 from modules.metrics.metrics import compute_all_scores
 from modules.optimizers import build_optimizer, build_lr_scheduler
 from modules.trainer_finetune_iu import PTrainer, FTrainer, PretrainTester, Tester
-from modules.utils import PretrainTestAnalysis, SetLogger, setup_seed
+from modules.utils import PretrainTestAnalysis, setup_arguments, setup_seed
 from models.model_pretrain_region_knowledge import Pretrain
 from models.model_pretrain_region_knowledge_local import LocalPretrain
 from models.model_pretrain_region_knowledge_global import GlobalPretrain
-from models.model_pretrain_region_knowledge_inference_iu import PretrainInference
+from models.model_pretrain_region_knowledge_inference import PretrainInference
 from models.model_finetune_region_knowledge_v1121 import FineTune
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
@@ -24,10 +24,8 @@ os.environ['TORCH_USE_CUDA_DSA'] = '1'
 os.environ['TOKENIZERS_PARALLELISM'] = 'true'
 import wandb
 
-os.environ["WANDB_API_KEY"] = '*********'
+# os.environ["WANDB_API_KEY"] = '*********'
 os.environ["WANDB_MODE"] = "offline"
-
-
 # wandb.login(key='************')
 
 
@@ -35,74 +33,7 @@ def main():
     # -------------------------------
     # load hyper-param
     # -------------------------------
-    parse = argparse.ArgumentParser()
-    # basic configuration
-    # pretrain: multi-granularity cross-modal alignment
-    # pretrain_inference: retrieve the historical similar cases for each medical image
-    # finetune: train text decoder based on historical similar cases
-    # inference: text generation for test dataset
-    parse.add_argument('--task', type=str, default='test',
-                       choices=['pretrain', 'pretrain_inference', 'finetune', 'test'])
-    # data configuration
-    parse.add_argument('--data_name', type=str, choices=['mimic_cxr', 'iu_xray'], default='mimic_cxr')
-    parse.add_argument('--mimic_cxr_ann_path', type=str,
-                       default='knowledge_encoder/mimic_cxr_annotation_sen_best_reports_keywords_20_all_components_with_fs_v0227.json')
-    parse.add_argument('--iu_xray_ann_path', type=str,
-                       default='/home/miao/data/Code/MSC-V1212-ablation-study/knowledge_encoder/iu_xray_annotation_sen_best_reports_keywords_20.json')
-    parse.add_argument('--text_decoder', type=str, choices=['r2gen', 'bert', 'cmn'], default='r2gen')
-    parse.add_argument('--visual_encoder', type=str, choices=['resnet101', 'ViT-B-32'], default='resnet101')
-    parse.add_argument('--tokenizer_model', type=str, choices=['wordlevel', 'wordpiece'], default='wordlevel')
-    parse.add_argument('--tokenizer_type', type=str, choices=['uncased', 'cased'], default='uncased')
-    parse.add_argument('--max_seq_len', type=int, default=100)
-    parse.add_argument('--freeze_image_encoder', action='store_true', help='whether freeze the image encoder')
-    parse.add_argument('--freeze_text_encoder', action='store_true', help='whether freeze the text encoder')
-    parse.add_argument('--is_save_checkpoint', action='store_true', help='whether save checkpoint')
-    # parse.add_argument('--freeze_image_encoder', action='store_true', help='whether freeze the image encoder')
-    # parse.add_argument('--freeze_text_encoder', action='store_false', help='whether freeze the text encoder')
-    # specific knowledge configuration
-    parse.add_argument('--sk_type', type=str, choices=['report', 'keywords'], default='keywords')
-    parse.add_argument('--sk_topk', type=int, default=5)
-    parse.add_argument('--is_add_indication', action='store_true', help='whether add indication section')
-    # parse.add_argument('--is_add_indication', action='store_false', help='whether add indication section')
-    parse.add_argument('--sk_fusion_strategy', type=str, choices=['mean', 'cat'], default='cat')
-    parse.add_argument('--sk_fusion_num_layers', type=int, default=1)
-    parse.add_argument('--sk_file_name', type=str, default='_v0107_')
-    # trainer configuration
-    parse.add_argument('--optim', type=str, choices=['AdamW', 'RAdam', "Adam"], default='RAdam',
-                       help='in the first stage, the optimization is AdamW with lr=5.0e-5, '
-                            'and in the second stage, optimizer is RAdam')
-    parse.add_argument('--lr_scheduler', type=str, choices=['StepLR', 'ReduceLROnPlateau'], default='ReduceLROnPlateau')
-    parse.add_argument('--lr', type=float, default=5.0e-5)  # 5.0e-5
-    parse.add_argument('--ft_monitor_metric', type=str, default='RCB')  # choices={metrics, RC, RB, RCB}
-    parse.add_argument('--epochs', type=int, default=100)
-    parse.add_argument('--batch_size', type=int, default=1)
-    parse.add_argument('--resume', type=str, help='whether to resume the training from existing checkpoints.')
-    parse.add_argument('--load', type=str, help='whether to load the pre-trained model.')
-    parse.add_argument('--version', type=str, default='long_sentence', help='the name of experiment')
-    # sk_type and align_type is the same.
-    parse.add_argument('--align_type', type=str, choices=['report', 'keywords'], default='keywords')
-    parse.add_argument('--align_loss', type=str, choices=['local', 'global', 'multi-level'], default='multi-level')
-    cmd = parse.parse_args()
-    cmd.config = 'config/finetune_config.yaml'
-    args = yaml.load(open(cmd.config), Loader=yaml.FullLoader)
-    cmd = vars(cmd)
-    args.update(cmd)
-    args['image_dir'] = args[f'{args["data_name"]}_image_dir']
-    args['ann_path'] = args[f'{args["data_name"]}_ann_path']
-    args['text_decoder'] = args['text_decoder'].lower()
-    args['device'] = 'cuda' if torch.cuda.is_available() else 'cpu'
-    args['result_dir'] = f'{args["result_dir"]}/{args["data_name"]}/{args["task"]}/{args["version"]}'
-    os.makedirs(args['result_dir'], exist_ok=True)
-
-    logger = SetLogger(f'{args["result_dir"]}/{args["task"]}_{args["text_decoder"]}_{args["sk_topk"]}.log', 'a')
-    if args['task'] in ['pretrain', 'pretrain_inference']:
-        args['monitor_mode'] = args['pt_monitor_mode']
-        args['monitor_metric'] = args['pt_monitor_metric']
-        args['lr_monitor_metric'] = args['pt_lr_monitor_metric']
-    else:
-        args['monitor_mode'] = args['ft_monitor_mode']
-        args['monitor_metric'] = args['ft_monitor_metric']
-        args['lr_monitor_metric'] = args['ft_lr_monitor_metric']
+    args, logger = setup_arguments()
     # -------------------------------
     # init wandb
     runner = wandb.init(
@@ -166,6 +97,10 @@ def main():
             test_loader_inc = FinetuneLoaderHaveIndication(args, tokenizer, split='test', shuffle=False, drop_last=False)
         test_loader_not_inc = FinetuneLoaderNotIndication(args, tokenizer, split='test', shuffle=False,
                                                           drop_last=False)
+
+    # -------------------------------
+    # record statistic of dataloader
+    # -------------------------------
     if args['task'] in ['pretrain', 'pretrain_inference']:
         print(f"train_data is {len(train_dataloader.dataset) if train_dataloader is not None else 'None'}, "
               f"val_data is {len(val_dataloader.dataset) if val_dataloader is not None else 'None'}, "
@@ -219,7 +154,7 @@ def main():
     else:  # finetune or test
         model = FineTune(args, tokenizer, args['data_name'])
     model = model.to(args['device'])
-    runner.watch(model, log='all')
+    # runner.watch(model, log='all')
     # -------------------------------
     print(f'finish instantiate model!, Trainable parameters:{str(model).split("Trainable parameters:")[1]}M')
     logger.info(f'finish instantiate model!, Trainable parameters:{str(model).split("Trainable parameters:")[1]}M')
@@ -255,16 +190,15 @@ def main():
         trainer.train()
     elif args['task'] == 'pretrain_inference':
         tester = PretrainTester(**kwarg)
-        specific_knowledge_data = tester.predict_iu_xray()
+        specific_knowledge_data = tester.predict_mimic_cxr()
         save_file_name = args['ann_path'].split('.json')[0] + f'{args["sk_file_name"]}{args["sk_topk"]}.json'
-        tester.get_specific_knowledge_iu_xray(specific_knowledge_data, save_file_name=save_file_name)
+        tester.get_specific_knowledge_mimic_cxr(specific_knowledge_data, save_file_name=save_file_name)
     elif args["task"] == 'finetune':
         trainer = FTrainer(**kwarg)
         trainer.train()
     else:  # inference
         trainer = Tester(**kwarg)
-        # trainer.test_for_each_sample_with_indication()
-        trainer.plot()
+        trainer.test()
     runner.finish()
 
 
